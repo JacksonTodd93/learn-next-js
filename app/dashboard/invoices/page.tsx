c347910833298d4fdff889fc0374d3e7150f09aa
Jackson Todd <jacksonttodd@gmail.com>
2024-01-25 10:45:44 -0500

    add metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,6 @@
 import Pagination from '@/app/ui/invoices/pagination';
 import { fetchInvoicesPages } from '@/app/lib/data';
+import { Metadata } from 'next';
 
 import Search from '@/app/ui/search';
 import Table from '@/app/ui/invoices/table';
@@ -7,6 +8,10 @@ import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
 
 export default async function Page({
   searchParams,
